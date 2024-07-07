3a211dd0ab6f392a6527edd943bdae19df60fcf0
jspverma <jsverma143@gmail.com>
2024-07-07 23:46:08 +0530

    Initial commit

--- solarhub/settings.py
+++ solarhub/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'auth'
+    'django.contrib.staticfiles'
 ]
 
 AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend']
